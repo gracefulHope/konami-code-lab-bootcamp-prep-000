ca3b38a713aea3f11409f4430a14fdddddee8357
Sara S <ewf.gloria@gmail.com>
2018-02-16 20:14:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,15 +10,8 @@ function init() {
     
 }
 
-function arraysEqual (array1, array2){
-  if (array1.length != array2.length){
-    return false
-  } else {
-    for (let i = 0; i < array1.length; i++){
-      if (array1[i] != array2[i]){
-        return false
-      }
-    }
-    return true
-  }
+let index = 0;
+
+function keyDownHandler (e){
+  
 }
