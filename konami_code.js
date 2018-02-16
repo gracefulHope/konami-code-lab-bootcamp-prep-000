f019ed1d026652bac2975f84dd5c09269a366082
Sara S <ewf.gloria@gmail.com>
2018-02-16 20:17:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  document.body.addEventListener('keydown', onKeyDownHandler()){
+  document.body.addEventListener('keydown', onKeyDownHandler(e))
 }
 
 let index = 0;
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
+ console.log(e.which)
+ console.log(e.detail)
  
   if (key === code[index]) {
     index++;
