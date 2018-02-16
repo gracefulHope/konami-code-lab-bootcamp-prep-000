d79d462ab3f3304baa111b6ed32246c659ceb562
Sara S <ewf.gloria@gmail.com>
2018-02-16 20:34:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0
+
 function init() {
-  document.body.addEventListener('keydown', onKeyDownHandler())
-}
-
-let index = 0;
-
-function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
  console.log(`Which key: ${e.which}`)
  console.log(`Key Detail: ${e.detail}`)
@@ -22,4 +19,6 @@ function onKeyDownHandler(e) {
   } else {
     index = 0;
   }
+})
 }
+
