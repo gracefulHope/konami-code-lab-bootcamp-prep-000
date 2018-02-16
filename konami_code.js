508b8cf9693521b9b200d8e3847ac5e339c46290
Sara S <ewf.gloria@gmail.com>
2018-02-16 20:08:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   document.body.addEventListener('keydown', function(e) {
+    
     alert('Congratulations - You did the Konami code!')
   })
     
 }
+
+function equalsArray
